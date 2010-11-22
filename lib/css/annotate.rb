5368d8f15e70b2f3cc6b38872a4685383b119d02
Assaf Arkin <assaf@labnotes.org>
2010-11-21 23:57:32 -0800

    Rack support! Mount css-annotate in your Web app to serve annotated CSS/SCSS/SASS along the originals. See README for examples.

--- lib/css/annotate.rb
+++ lib/css/annotate.rb
@@ -2,48 +2,73 @@ require "haml"
 require "sass/engine"
 require "erb"
 require "cgi"
+require "rack"
 
 module CSS
   class Annotate
 
-    def initialize(filename, options = {})
-      @filename = filename
-      @options = options.clone
-      paths = @options[:load_paths] || []
-      paths.push File.dirname(@filename)
+    # @param [Hash, String, nil] options Hash with options, just the path, or
+    # nil
+    # @options options [String] path Path to your CSS/SCSS/SASS source files
+    # @options options [Array<String>] load_paths Additional paths for loading
+    # CSS files
+    # @options options [Boolean] all If nil, true show all styles (with buttons to
+    # show only commented); if false show only commented styles.
+    def initialize(options = nil)
+      @options = String === options ? { :path=>options } : options ? options.clone : {}
+      @options[:path] ||= "public/stylesheets/"
+      paths =[@options[:path]]
       paths.concat Compass::Frameworks::ALL.map { |framework| framework.stylesheets_directory } if defined?(Compass)
       @options[:load_paths] = paths
-      @options[:syntax] ||= guess_syntax(filename)
+      @options[:all] = true unless @options.has_key?(:all)
     end
 
-    attr_reader :filename, :options, :rows
+    attr_reader :options, :rows
 
-    def annotate
-      engine = Sass::Engine.new(IO.read(filename), options)
+    # Annotate the named file. Sets and returns rows.
+    def annotate(filename)
+      engine = Sass::Engine.new(IO.read(filename), options.merge(:syntax=>guess_syntax(filename)))
       tree = engine.to_tree
       tree.perform!(Sass::Environment.new)
       resolve_rules tree
       @rows = to_rows(tree)
     end
 
-    def h(raw)
-      CGI.escapeHTML(raw.to_s)
-    end
-
-    def to_html
-      rows = annotate
+    # Annotate the named file, returns HTML document.
+    def to_html(filename)
+      rows = annotate(filename)
       ERB.new(IO.read(File.dirname(__FILE__) + "/annotate/template.erb")).result(binding)
     rescue Sass::SyntaxError=>error
       error = Sass::SyntaxError.exception_to_css error, @options.merge(:full_exception=>true)
       ERB.new(IO.read(File.dirname(__FILE__) + "/annotate/template.erb")).result(binding)
     end
 
+    # ERB template uses this to HTML escape content.
+    def h(raw)
+      CGI.escapeHTML(raw.to_s)
+    end
+
+    # ERB template uses this to obtain our own stylesheet.
     def styles
       Sass::Engine.new(IO.read(File.dirname(__FILE__) + "/annotate/style.scss"), :syntax=>:scss).render
     end
-    
+   
+    # EBB template uses this to determine if it should show all, or just
+    # commented, styles. 
     def all?
       !!options[:all]
+    end
+
+    def call(env)
+      request = Rack::Request.new(env)
+      filename = request.path_info.split("/").last
+      filename = File.expand_path(filename, @options[:path]) if filename
+      if filename && File.exist?(filename)
+        html = to_html(filename)
+        [200, { "Content-Type"=>"text/html", "Content-Length"=>html.length.to_s }, [html]]
+      else
+        [404, { "Content-Type"=>"text/plain" }, ["Could not find '#{filename}'"]]
+      end
     end
 
   protected
